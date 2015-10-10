f32e9fc163d50dd2e0e70beb32d8d72be2d98692
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-10-10 06:47:24 -0500

    Added documentation/ fixed api call

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
+import decimal
 
 class Text(object):
+    """
+    Create a Text object with the Walmart text as the argument.
+    Extracts the product name.
+    """
     def __init__(self, text):
         self.text = text
 
@@ -8,11 +13,15 @@ class Text(object):
         return self.text.find("item: ")
 
 def Walmart(object):
+    """
+    Create Walmart object with the item name as the argument.
+    Call get_price on the object to get the price.
+    """
     def __init__(self, item):
         self.item = item
 
-    def get_product_id(self):
-        search = requests.get('http://api.walmartlabs.com/v1/search?apiKey={apiKey}&lsPublisherId={Your LinkShare Publisher Id}&query={0}'.format(self.item)
+    def _get_product_id(self):
+        search = requests.get('http://api.walmartlabs.com/v1/search?apiKey={0}&lsPublisherId={1}&query={2}'.format(api_key, linkshare_publisher_id, self.item))
 
         if search.status_code == requests.codes.ok and 'json' in r.headers['content-type']:
             product_json = search.json()
@@ -31,13 +40,13 @@ def Walmart(object):
             return -2
 
     def get_price(self):
-        self.item_id = self.get_product_id()
+        self.item_id = self._get_product_id()
 
         if self.item_id <= 0:
             return self.item_id
 
         else:
-            lookup = requests.get('http://api.walmartlabs.com/v1/items/12417832?apiKey={apiKey}&lsPublisherId={Your LinkShare Publisher Id}&format=json')
+            lookup = requests.get('http://api.walmartlabs.com/v1/items/{0}?apiKey={1}&lsPublisherId={2}&format=json'.format(self.item, api_key, linkshare_publisher_id))
 
         if lookup.status_code == requests.codes.ok and 'json' in r.headers['content-type']:
             item_json = lookup.json()
@@ -48,7 +57,7 @@ def Walmart(object):
         price = item_json['salePrice']
 
         if price > 0:
-            return price
+            return decimal.Decimal(price) / 100   # API returns cost in cents, this converts to dollars
 
         else:
             return -4
