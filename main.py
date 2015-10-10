0c219728e678cb18b72090747b93a1dc0f1da6ac
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-10-10 06:06:08 -0500

    Added get_product_id method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import request
+import requests
 
 class Text(object):
     def __init__(self, text):
@@ -6,4 +6,27 @@ class Text(object):
 
     def get_item():
         return self.text.find("item: ")
+
+def Walmart(object):
+    def __init__(self, item):
+        self.item = item
+
+    def get_product_id():
+        search = requests.get('http://api.walmartlabs.com/v1/search?apiKey={apiKey}&lsPublisherId={Your LinkShare Publisher Id}&query={0}'.format(self.item)
+
+        if search.status_code == requests.codes.ok and 'json' in r.headers['content-type']:
+            product_json = search.json()
+
+        else:
+            return -1
+
+        item_list = product_json['items']
+        item = item_list[0]
+        item_id = item['itemId']
+
+        if item_id > 0:
+            return item_id
+
+        else:
+            return -2
 
